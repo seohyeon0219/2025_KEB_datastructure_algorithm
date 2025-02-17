6b5f83c346c952d6a79f53eff9e2c800c9751dcf
Kimseohyeon <seohyeon0219@naver.com>
2025-02-17 10:52:27 +0900

    v0.3 day06 is_even

--- day06.py
+++ day06.py
@@ -1,22 +1,19 @@
-v0.2 day06 bit operation
 def is_even(n) -> bool:
     '''
     짝수 판정 함수
     :param n: 판정할 정수
     :return: 짝수면 True, 홀수면 False
     '''
-    if n%2==0:
-        return True
-    return False
 
-a = 10 # 0000 1010
-b = 11 # 0000 1011
-# bit operation
 
-print(a and b)
-print(a % b) # 0000 1010 (둘 다 1일때만 1) -> 10 출력
-print(a | b) # 11 출력
-print(a ^ b) # 0001
+    return not n & 1
+    # 비트 연산(홀수는 무조건 마지막 비트가 1로 끝남)으로 짝수 판정 가능, 11을 입력하면 1011 & 0001
+    # if n % 2==0:
+    #     return True
+    # return False
+
 
 n = int(input())
 print(is_even(n))
+
+
