2c2b6922790e02ed36933c9034de8ed769a738fb
Kimseohyeon <seohyeon0219@naver.com>
2025-02-17 10:53:10 +0900

    v0.4 day06 recursion

--- day06.py
+++ day06.py
@@ -1,19 +1,10 @@
-def is_even(n) -> bool:
-    '''
-    짝수 판정 함수
-    :param n: 판정할 정수
-    :return: 짝수면 True, 홀수면 False
-    '''
-
-
-    return not n & 1
-    # 비트 연산(홀수는 무조건 마지막 비트가 1로 끝남)으로 짝수 판정 가능, 11을 입력하면 1011 & 0001
-    # if n % 2==0:
-    #     return True
-    # return False
+def dec_oct(n) -> int:
+    if n == 0:
+        return ""
+    else:
+        return dec_oct(n // 8) + str(n % 8)
 
 
 n = int(input())
-print(is_even(n))
-
-
+print(dec_oct(n))
+# print(n, oct(n))
