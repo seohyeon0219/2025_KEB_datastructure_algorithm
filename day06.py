937b98df3f525f5d028bcff2308bb28b96d8b827
Kimseohyeon <seohyeon0219@naver.com>
2025-02-17 16:23:01 +0900

    v1.1 day06 array

--- day06.py
+++ day06.py
@@ -1,43 +1,9 @@
-memo = dict()
+import numpy as np
 
-def fibonacci_recursion(n) -> int:
-    """
-    피보나치 수 계산함수 (재귀함수 버전)
-    :param n:
-    :return: 피보나치 계산 결과 값
-    """
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
+narray = np.array([1, 3.1, 2, 9])
+print(type(narray), type(narray[2]), type(narray[3]))
 
-
-def fibonacci_loop(n) -> int:
-    """
-    피보나치 수 계산함수 (반복문 버전)
-    :param n:
-    :return: 피보나치 계산 결과 값
-    """
-    n_list=[0 ,1]
-    for i in range(n+1):
-        n_list.append(n_list[i] + n_list[i + 1])
-
-    return n_list[n]
-
-
-def fibonacci_memo(n) -> int:
-    if n in memo:  # 딕셔너리에 이미 계산된 결과가 있으면 그 값을 리턴
-        return memo[n]
-    elif n <= 1:  # 0이나 1이 오면 그 값을 바로 리턴
-        return n
-    else:
-        memo[n] = fibonacci_memo(n-2) + fibonacci_memo(n-1)  # 딕셔너리에 계산된 결과 값이 없을 경우 딕셔너리에 추가
-        return memo[n]
-
-
-n = int(input())
-print(fibonacci_loop(n))
-print(fibonacci_recursion(n))
-print(fibonacci_memo(n))
+array = [9, -11, '8', 7]
+print(array[0], array[1], array[2], array[3])
+print(type(array), type(array[2]), type(array[3]))
+print(id(array[0]),id(array[1]),id(array[2]),id(array[3]))
