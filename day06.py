494feed16a2abfbe557acdcd8dee32451a627297
Kimseohyeon <seohyeon0219@naver.com>
2025-02-17 16:20:19 +0900

    v0.9 day06 poly

--- day06.py
+++ day06.py
@@ -1,28 +1,24 @@
 def print_poly(f_x, t_x) -> str:
-    #term = len(f_x) - 1
     poly_expression = "f(x) = "
 
     for i in range(len(fx)):
         coefficient = f_x[i]
         term = t_x[i]
 
-        if coefficient >= 0:
+        if coefficient >= 0 and i != 0:  # feat. csh
             poly_expression = poly_expression + "+"
         poly_expression = poly_expression + f'{coefficient}x^{term} '
-        #term = term - 1
 
     return poly_expression
 
 
 def calculation_poly(x_value, f_x, t_x) -> int:
     return_value = 0
-    # term = len(f_x) - 1
 
     for i in range(len(fx)):
         coefficient = f_x[i]
         term = t_x[i]
         return_value += coefficient * pow(x_value, term)
-        # term = term - 1
 
     return return_value
 
