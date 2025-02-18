c002ad61a1f448fbbcba1d6bda74a26515cf539f
Kimseohyeon <seohyeon0219@naver.com>
2025-02-18 11:22:28 +0900

    v1.9 day07 linked list

--- day07.py
+++ day07.py
@@ -1,17 +1,24 @@
-def a(i):
-    j = 9
-    print(i, j)
+class Node:
+    def __init__(self,data,next=None):
+        self.data = data
+        self.next = next
 
+class LinkedList:
+    def __int__(self):
+        self.head = None
 
-def b(k):
-    a(1)
-    print(k)
-
-
-def main():
-    print("start")
-    b(3)
+    def append(self,data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next: # if node exist
+            current = current.next # move
+        current.next = Node(data)
 
 
 if __name__ == "__main__":
-    main()
+    l = LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
