b4c1b773f9447bd13ea26f160a9bdf3ca9463c01
Kimseohyeon <seohyeon0219@naver.com>
2025-02-18 13:06:31 +0900

    v2.3 day07 linked list

--- day07.py
+++ day07.py
@@ -1,45 +1,35 @@
+import random
+
+
 class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
+
 
 class LinkedList:
     def __init__(self):
         self.head = None
 
     def append(self, data):
-        if not self.head:
-            self.head = Node(data)
-            return
-        current = self.head
-        while current.next:  # if next node exist
-            current = current.next  # move
         current.next = Node(data)
 
     def search(self, target) -> bool:
-        current = self.head
-        while current.next:
-            if current.data == target:
-                return True
-            else:
-                current = current.next
         return False
 
 
     def __str__(self):
-        node = self.head
-        while node is not None:
-            print(node.data)
-            node = node.next
         return "end"
 
 
 
 if __name__ == "__main__":
     l = LinkedList()
-    l.append(7)
-    l.append(-11)
-    l.append(8)
-    print(l)
-    print(l.search(999))
-    print(l.search(-11))
+    i = 0
+    while i < 20:
+        n = random.randint(1, 20)
+        l.append(n)
+        print(n, end=' ')
+        i = i + 1
+    #print(l)
+    print(l.search(10))
