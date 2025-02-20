df9aeb6ac7bf00f50a590fd1cf94b735f05d25dc
Kimseohyeon <seohyeon0219@naver.com>
2025-02-20 13:24:59 +0900

    v3.8 day09 tree

--- day09.py
+++ day09.py
@@ -38,6 +38,33 @@ def post_order(node):
     print(f"{node.data} ", end='')
 
 
+def delete(root, value):
+    if root is None:
+        return root
+    if value < root.data:
+        root.left = delete(root.left, value)
+    elif value > root.data:
+        root.right = delete(root.right, value)
+    else:
+        if root.left is None and root.right is None:
+            return None
+        elif root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+        else:  # 자식이 두 개인 경우
+            root.data = find_min(root.right).data
+            root.right = delete(root.right, root.data)
+    return root
+
+
+def find_min(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current
+
+
 if __name__ == "__main__":
     numbers = [10, 15, 8, 3, 9]
     root = None
@@ -49,8 +76,20 @@ if __name__ == "__main__":
     post_order(root)
 
     print()
-    find_number = int(input())
-    if search(root, find_number):
-        print(f"{find_number}을(를) 찾았습니다")
+    print(root.data)
+    # find_number = int(input())
+    # if search(root, find_number):
+    #     print(f"{find_number}을(를) 찾았습니다")
+    # else:
+    #     print(f"{find_number}이(가) 존재하지 않습니다")
+
+    delete_number = int(input())
+    if search(root, delete_number):
+        root = delete(root, delete_number)
+        print(f"{delete_number} 삭제 완료")
     else:
-        print(f"{find_number}이(가) 존재하지 않습니다")
+        print(f"{delete_number}은(는) 트리에 존재하지 않습니다.")
+
+    post_order(root)
+    print()
+    print(root.data)
