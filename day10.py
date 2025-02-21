605cbd23a064cf71aada6327a43770785babc3c0
Kimseohyeon <seohyeon0219@naver.com>
2025-02-21 13:36:35 +0900

    v4.8 day10 sort

--- day10.py
+++ day10.py
@@ -1,10 +1,15 @@
 def bubble_sort(l):
     for i in range(len(l) - 1):
+        no_swap = True
         for j in range(len(l) - 1 - i):
             if l[j] > l[j+1]:
                 l[j], l[j + 1] = l[j+1], l[j]
-            #print(j, end=' ')
+                no_swap = False
+                print(j, end=' ')
+        if no_swap:
+            return l
     return l
 
 
 print(bubble_sort([33, 8, -11, 9, 1]))
+print(bubble_sort([13, 15, 20, 99, 100]))
