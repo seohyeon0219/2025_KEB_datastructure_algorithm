e9958b5d473de7c4c1ca30de3c87097f5afa1a3e
Kimseohyeon <seohyeon0219@naver.com>
2025-02-21 13:38:05 +0900

    v4.9 day10 sort

--- day10.py
+++ day10.py
@@ -1,3 +1,14 @@
+def insertion_sort(l):
+    for i in range(1, len(l)):
+        value = l[i]
+        while i > 0 and l[i-1] > value:
+            l[i] = l[i-1]
+            i = i - 1
+            print(i, end=' ')
+        l[i] = value
+    return l
+
+
 def bubble_sort(l):
     for i in range(len(l) - 1):
         no_swap = True
@@ -13,3 +24,5 @@ def bubble_sort(l):
 
 print(bubble_sort([33, 8, -11, 9, 1]))
 print(bubble_sort([13, 15, 20, 99, 100]))
+print(insertion_sort([33, 8, -11, 9, 1]))
+print(insertion_sort([13, 15, 20, 99, 100]))
